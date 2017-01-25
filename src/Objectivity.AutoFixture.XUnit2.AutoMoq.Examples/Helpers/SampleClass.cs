f6a391ffbd7c76f82a6c4db80661b3ff1d8dab56
Michal Jankowski <mjankowski@objectivity.co.uk>
2017-01-25 07:38:22 +0100

    Change examples

--- src/Objectivity.AutoFixture.XUnit2.AutoMoq.Examples/Helpers/SampleClass.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Objectivity.AutoFixture.XUnit2.AutoMoq.Examples.Helpers
-{
-    public class SampleClass
-    {
-        public string NotVirtualProperty { get; set; }
-
-        public virtual string VirtualProperty { get; set; }
-    }
-}
