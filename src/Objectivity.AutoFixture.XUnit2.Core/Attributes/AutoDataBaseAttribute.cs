adfe0c48ba3219ae3d6be9a39e595f45f3ec1839
smg <semagerasimov@gmail.com>
2019-07-28 13:44:48 +0700

    Add ability to provide own customization in AutoMockData

--- src/Objectivity.AutoFixture.XUnit2.Core/Attributes/AutoDataBaseAttribute.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core/Attributes/AutoDataBaseAttribute.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Reflection;
     using global::AutoFixture;
+    using global::AutoFixture.Kernel;
     using Objectivity.AutoFixture.XUnit2.Core.Common;
     using Objectivity.AutoFixture.XUnit2.Core.Customizations;
     using Objectivity.AutoFixture.XUnit2.Core.Providers;
@@ -25,16 +26,43 @@
         /// </summary>
         public bool IgnoreVirtualMembers { get; set; }
 
+        /// <summary>
+        ///  Gets or sets array of <see cref="ICustomization"/>s or <see cref="ISpecimenBuilder"/> to apply.
+        /// </summary>
+        public Type[] Customizations { get; set; }
+
         public IAutoFixtureAttributeProvider Provider { get; }
 
         public override IEnumerable<object[]> GetData(MethodInfo testMethod)
         {
             this.Fixture.Customize(new AutoDataCommonCustomization(this.IgnoreVirtualMembers));
             this.Customize(this.Fixture);
+            this.ApplyCustomizations();
 
             return this.Provider.GetAttribute(this.Fixture).GetData(testMethod);
         }
 
         protected abstract IFixture Customize(IFixture fixture);
+
+        private void ApplyCustomizations()
+        {
+            foreach (var type in this.Customizations)
+            {
+                var instance = Activator.CreateInstance(type);
+                switch (instance)
+                {
+                    case ISpecimenBuilder specimenBuilder:
+                        this.Fixture.Customizations.Add(specimenBuilder);
+                        break;
+
+                    case ICustomization customization:
+                        this.Fixture.Customize(customization);
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Provided customization is not of supported type.");
+                }
+            }
+        }
     }
 }
