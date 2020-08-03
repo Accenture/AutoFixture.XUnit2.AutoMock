04649f433c46d748ba9217d804013a64b9d3df62
piotrzajac <piotr.zajac@live.com>
2020-08-03 19:11:39 +0200

    Skip data generation when data already specified through inline or member attribute

--- src/Objectivity.AutoFixture.XUnit2.Core.Tests/Attributes/AutoDataAdapterAttributeTests.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core.Tests/Attributes/AutoDataAdapterAttributeTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace Objectivity.AutoFixture.XUnit2.Core.Tests.Attributes
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Reflection;
     using FluentAssertions;
     using global::AutoFixture;
     using Objectivity.AutoFixture.XUnit2.Core.Attributes;
@@ -8,10 +11,11 @@
 
     [Collection("AutoDataAdapterAttribute")]
     [Trait("Category", "Attributes")]
+    [SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "Test objects")]
     public class AutoDataAdapterAttributeTests
     {
-        [Fact(DisplayName = "GIVEN fixture WHEN constructor is invoked THEN passed fixture is being adapted")]
-        public void GivenFixture_WhenConstructorIsInvoked_ThenPassedFixtureIsBeingAdapted()
+        [Fact(DisplayName = "GIVEN fixture WHEN constructor is invoked THEN passed fixture is being adapted and inline values collection is empty")]
+        public void GivenFixture_WhenConstructorIsInvoked_ThenPassedFixtureIsBeingAdaptedAndInlineValuesCollectionIsEmpty()
         {
             // Arrange
             IFixture fixture = new Fixture();
@@ -21,6 +25,7 @@
 
             // Assert
             attribute.AdaptedFixture.Should().Be(fixture);
+            attribute.InlineValues.Should().BeEmpty();
         }
 
         [Fact(DisplayName = "GIVEN uninitialized fixture WHEN constructor is invoked THEN exception is thrown")]
@@ -32,6 +37,59 @@
             // Act
             // Assert
             Assert.Throws<ArgumentNullException>(() => new AutoDataAdapterAttribute(fixture));
+        }
+
+        [Fact(DisplayName = "GIVEN test data with instance WHEN GetData called THEN auto data generation skipped")]
+        public void GivenTestDataWithInstance_WhenGetDataCalled_ThenAutoDataGenerationSkipped()
+        {
+            // Arrange
+            IFixture fixture = new Fixture();
+            var attribute = new AutoDataAdapterAttribute(fixture, SpecificTestClass.Create());
+            var methodInfo = typeof(AutoDataAdapterAttributeTests).GetMethod(nameof(this.TestMethodWithAbstractTestClass), BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Act
+            var data = attribute.GetData(methodInfo);
+
+            // Assert
+            data.Should().HaveCount(1)
+                .And.Subject.First().Should().HaveCount(methodInfo.GetParameters().Length)
+                .And.NotContainNulls();
+        }
+
+        [Fact(DisplayName = "GIVEN empty test data WHEN GetData called THEN auto data generation throws exception")]
+        public void GivenEmptyTestData_WhenGetDataCalled_ThenAutoDataGenerationSkipped()
+        {
+            // Arrange
+            IFixture fixture = new Fixture();
+            var attribute = new AutoDataAdapterAttribute(fixture);
+            var methodInfo = typeof(AutoDataAdapterAttributeTests).GetMethod(nameof(this.TestMethodWithAbstractTestClass), BindingFlags.Instance | BindingFlags.NonPublic);
+
+            // Act
+            Action data = () => attribute.GetData(methodInfo);
+
+            // Assert
+            data.Should().Throw<Exception>();
+        }
+
+        protected string TestMethodWithAbstractTestClass(SpecificTestClass instance, string text)
+        {
+            return $"{instance}: {text}";
+        }
+
+        public abstract class AbstractTestClass
+        {
+        }
+
+        public class SpecificTestClass : AbstractTestClass
+        {
+            private SpecificTestClass()
+            {
+            }
+
+            public static AbstractTestClass Create()
+            {
+                return new SpecificTestClass();
+            }
         }
     }
 }
--- src/Objectivity.AutoFixture.XUnit2.Core/Attributes/AutoDataAdapterAttribute.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core/Attributes/AutoDataAdapterAttribute.cs
@@ -1,19 +1,68 @@
 ﻿namespace Objectivity.AutoFixture.XUnit2.Core.Attributes
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using global::AutoFixture;
+    using global::AutoFixture.Kernel;
     using global::AutoFixture.Xunit2;
     using Objectivity.AutoFixture.XUnit2.Core.Common;
+    using Objectivity.AutoFixture.XUnit2.Core.Comparers;
 
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     internal sealed class AutoDataAdapterAttribute : AutoDataAttribute
     {
-        public AutoDataAdapterAttribute(IFixture fixture)
+        public AutoDataAdapterAttribute(IFixture fixture, params object[] inlineValues)
             : base(() => fixture)
         {
             this.AdaptedFixture = fixture.NotNull(nameof(fixture));
+            this.InlineValues = inlineValues ?? Array.Empty<object>();
         }
 
         public IFixture AdaptedFixture { get; }
+
+        public IReadOnlyCollection<object> InlineValues { get; }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (testMethod == null)
+            {
+                throw new ArgumentNullException(nameof(testMethod));
+            }
+
+            // This is an extension of AutoDataAttribute.GetData method
+            // with the ability to skip already provided inline values
+            var specimens = new List<object>(this.InlineValues);
+            var parameters = testMethod.GetParameters();
+            foreach (var p in parameters.Skip(this.InlineValues.Count))
+            {
+                this.CustomizeFixture(p);
+
+                var specimen = this.Resolve(p);
+                specimens.Add(specimen);
+            }
+
+            return new[] { specimens.ToArray() };
+        }
+
+        private void CustomizeFixture(ParameterInfo p)
+        {
+            var customizeAttributes = p.GetCustomAttributes()
+                .OfType<IParameterCustomizationSource>()
+                .OrderBy(x => x, new CustomizeAttributeComparer());
+
+            foreach (var ca in customizeAttributes)
+            {
+                var c = ca.GetCustomization(p);
+                this.AdaptedFixture.Customize(c);
+            }
+        }
+
+        private object Resolve(ParameterInfo p)
+        {
+            var context = new SpecimenContext(this.AdaptedFixture);
+            return context.Resolve(p);
+        }
     }
 }
