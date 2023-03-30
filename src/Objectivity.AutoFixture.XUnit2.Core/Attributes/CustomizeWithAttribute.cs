3a285e4cc0e6f166890ab5b17fc41e940de1115b
Piotr Zajac <piotr.zajac@live.com>
2023-03-30 07:01:52 +0200

    - Introduce generic version of CustomizeWithAttribute - Make CustomizeWithAttribute classes extendable by removing sealed keyword - Introduce logic of including parameter type when constructing customization through CustomizeWithAttribute

--- src/Objectivity.AutoFixture.XUnit2.Core/Attributes/CustomizeWithAttribute.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core/Attributes/CustomizeWithAttribute.cs
@@ -1,13 +1,16 @@
 ﻿namespace Objectivity.AutoFixture.XUnit2.Core.Attributes
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Reflection;
 
     using global::AutoFixture;
     using global::AutoFixture.Xunit2;
 
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)]
-    public sealed class CustomizeWithAttribute : CustomizeAttribute
+    [SuppressMessage("Performance", "CA1813:Avoid unsealed attributes", Justification = "This attribute should be extendable by inheritance.")]
+    public class CustomizeWithAttribute : CustomizeAttribute
     {
         public CustomizeWithAttribute(Type type, params object[] args)
         {
@@ -30,9 +33,24 @@
 
         public object[] Args { get; }
 
+        public bool IncludeParameterType { get; set; }
+
         public override ICustomization GetCustomization(ParameterInfo parameter)
         {
-            return Activator.CreateInstance(this.Type, this.Args) as ICustomization;
+            var args = this.Args;
+            if (this.IncludeParameterType)
+            {
+                if (parameter is null)
+                {
+                    throw new ArgumentNullException(nameof(parameter));
+                }
+
+                args = new object[] { parameter.ParameterType }
+                    .Concat(this.Args ?? Array.Empty<object>())
+                    .ToArray();
+            }
+
+            return Activator.CreateInstance(this.Type, args) as ICustomization;
         }
     }
 }
