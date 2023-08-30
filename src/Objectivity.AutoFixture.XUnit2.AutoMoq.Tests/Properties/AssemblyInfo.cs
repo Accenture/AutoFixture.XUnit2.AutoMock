997893ac21a470cb214f8f11264c6615d3c52444
Piotr Zając <piotr.zajac@live.com>
2023-08-30 10:25:20 +0200

    Github workflow (#44)
    * Replace AppVeyor build definition with GitHub workflows
    * Enable GitHub workflow manual trigger and introduce ability to select environment and package
    * Use windows agents to support old .NET Framework
    * Update expected dotnet version from 6 to 7
    * Update analyzers
    * Ignore S6608 rule in tests
    * Address IDE0005 analyzer on the build issue: https://github.com/dotnet/aspnetcore/issues/47912
    * Use gitversion for automatic Versioning
    * Automate pushing tags when publishing packages
    
    ---------
    
    Co-authored-by: krzysztofkaczorowski <krzysiek.kacz@gmail.com>
    Co-authored-by: Krzysztof Kaczorowski <kkaczorowski@objectivity.co.uk>

--- src/Objectivity.AutoFixture.XUnit2.AutoMoq.Tests/Properties/AssemblyInfo.cs
+++ src/Objectivity.AutoFixture.XUnit2.AutoMoq.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,4 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Objectivity.AutoFixture.XUnit2.AutoMoq.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Objectivity.AutoFixture.XUnit2.AutoMoq.Tests")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
