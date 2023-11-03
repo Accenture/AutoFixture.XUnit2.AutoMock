47a498e447dd899d522c08fcc552c68b2cd944ad
Piotr Zajac <piotr.zajac@live.com>
2023-11-03 19:12:13 +0100

    Remove constructor with types not allowed as attribute arguments.

--- src/Objectivity.AutoFixture.XUnit2.Core/Attributes/FromRangeAttribute.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core/Attributes/FromRangeAttribute.cs
@@ -7,11 +7,6 @@
     [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)]
     public sealed class FromRangeAttribute : BuildWithAttribute<RandomRangedNumberBuilder>
     {
-        public FromRangeAttribute(sbyte minimum, sbyte maximum)
-            : this((object)minimum, maximum)
-        {
-        }
-
         public FromRangeAttribute(byte minimum, byte maximum)
             : this((object)minimum, maximum)
         {
@@ -22,27 +17,12 @@
         {
         }
 
-        public FromRangeAttribute(ushort minimum, ushort maximum)
-            : this((object)minimum, maximum)
-        {
-        }
-
         public FromRangeAttribute(int minimum, int maximum)
             : this((object)minimum, maximum)
         {
         }
 
-        public FromRangeAttribute(uint minimum, uint maximum)
-            : this((object)minimum, maximum)
-        {
-        }
-
         public FromRangeAttribute(long minimum, long maximum)
-            : this((object)minimum, maximum)
-        {
-        }
-
-        public FromRangeAttribute(ulong minimum, ulong maximum)
             : this((object)minimum, maximum)
         {
         }
@@ -53,11 +33,6 @@
         }
 
         public FromRangeAttribute(float minimum, float maximum)
-            : this((object)minimum, maximum)
-        {
-        }
-
-        public FromRangeAttribute(decimal minimum, decimal maximum)
             : this((object)minimum, maximum)
         {
         }
