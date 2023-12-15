7710e33f988ef660d440ee5ad6981cd16ffd0ae1
Piotr Zając <piotr.zajac@live.com>
2023-12-15 11:54:23 +0100

    Introduce NegativeValuesRequestFactory (#94)
    * Introduce NegativeValuesRequestFactory

---
+++ src/Objectivity.AutoFixture.XUnit2.Core/Factories/IFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Objectivity.AutoFixture.XUnit2.Core.Factories
+{
+    internal interface IFactory<in TInput, out TOutput>
+    {
+        TOutput Create(TInput input);
+    }
+}
---
+++ src/Objectivity.AutoFixture.XUnit2.Core/Factories/NegativeValuesRequestFactory.cs
@@ -0,0 +1,76 @@
+﻿namespace Objectivity.AutoFixture.XUnit2.Core.Factories
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    using global::AutoFixture;
+    using global::AutoFixture.Kernel;
+
+    using Objectivity.AutoFixture.XUnit2.Core.Common;
+    using Objectivity.AutoFixture.XUnit2.Core.Requests;
+
+    internal class NegativeValuesRequestFactory : IFactory<Type, object>
+    {
+        private static readonly decimal DecimalEpsilon = new(1, 0, 0, true, 28);
+
+        public object Create(Type input)
+        {
+            return input.NotNull(nameof(input)).IsEnum
+                ? GetRequestForEnum(input)
+                : GetRequestForNumericType(input);
+        }
+
+        private static FixedValuesRequest GetRequestForEnum(Type type)
+        {
+            var values = Type.GetTypeCode(type) switch
+            {
+                TypeCode.SByte => GetEnumNegativeValues<sbyte>(type),
+                TypeCode.Int16 => GetEnumNegativeValues<short>(type),
+                TypeCode.Int32 => GetEnumNegativeValues<int>(type),
+                TypeCode.Int64 => GetEnumNegativeValues<long>(type),
+                _ => Array.Empty<object>(),
+            };
+
+            if (values.Length == 0)
+            {
+                throw GetObjectCreationException();
+            }
+
+            return new FixedValuesRequest(type, values);
+        }
+
+        private static object[] GetEnumNegativeValues<T>(Type type)
+            where T : struct, IComparable
+        {
+            var zero = Convert.ChangeType(0, typeof(T), CultureInfo.InvariantCulture);
+            return Enum.GetValues(type)
+                .Cast<T>()
+                .Where(x => x.CompareTo(zero) < 0)
+                .Select(x => Enum.ToObject(type, x))
+                .ToArray();
+        }
+
+        private static RangedNumberRequest GetRequestForNumericType(Type type)
+        {
+            var (minimum, maximum) = Type.GetTypeCode(type) switch
+            {
+                TypeCode.Decimal => ((object, object))(decimal.MinValue, DecimalEpsilon),
+                TypeCode.Double => (double.MinValue, -double.Epsilon),
+                TypeCode.Int16 => (short.MinValue, (short)-1),
+                TypeCode.Int32 => (int.MinValue, -1),
+                TypeCode.Int64 => (long.MinValue, -1L),
+                TypeCode.SByte => (sbyte.MinValue, (sbyte)-1),
+                TypeCode.Single => (float.MinValue, -float.Epsilon),
+                _ => throw GetObjectCreationException(),
+            };
+
+            return new RangedNumberRequest(type, minimum, maximum);
+        }
+
+        private static ObjectCreationException GetObjectCreationException()
+        {
+            return new ObjectCreationException("The value could not be created. Probably attribute is specified for a type that does not accept negative values.");
+        }
+    }
+}
