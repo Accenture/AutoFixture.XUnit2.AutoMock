0355b4c23afe4fed978bb0075408ca0fec6b91ae
Piotr Zajac <piotr.zajac@live.com>
2024-01-15 16:20:17 +0100

    Suppress code inspections

--- src/Objectivity.AutoFixture.XUnit2.AutoFakeItEasy.Tests/IFakeObjectUnderTest.cs
+++ src/Objectivity.AutoFixture.XUnit2.AutoFakeItEasy.Tests/IFakeObjectUnderTest.cs
@@ -1,7 +1,10 @@
 ﻿namespace Objectivity.AutoFixture.XUnit2.AutoFakeItEasy.Tests
 {
+    using System.Diagnostics.CodeAnalysis;
+
     public interface IFakeObjectUnderTest
     {
+        [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Required for test.")]
         public string StringProperty { get; set; }
     }
 }
