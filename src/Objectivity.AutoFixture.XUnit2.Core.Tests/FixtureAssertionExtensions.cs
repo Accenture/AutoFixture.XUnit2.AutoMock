8d7a9dea879e8ff2dd975abd90de42d6d7f9153c
Piotr Zajac <piotr.zajac@live.com>
2025-04-24 09:42:56 +0200

    Introduce methods for preconditions

--- src/Objectivity.AutoFixture.XUnit2.Core.Tests/FixtureAssertionExtensions.cs
+++ src/Objectivity.AutoFixture.XUnit2.Core.Tests/FixtureAssertionExtensions.cs
@@ -4,8 +4,7 @@
     using System.Linq;
 
     using global::AutoFixture;
-
-    using JetBrains.Annotations;
+    using global::AutoFixture.Kernel;
 
     using Objectivity.AutoFixture.XUnit2.Core.SpecimenBuilders;
 
@@ -13,39 +12,40 @@
 
     internal static class FixtureAssertionExtensions
     {
-        [AssertionMethod]
         internal static void ShouldNotThrowOnRecursion(this IFixture fixture)
         {
             // Ensure there is no behavior for throwing exception on recursive structures.
-            Assert.DoesNotContain(fixture.Behaviors, b => b is ThrowingRecursionBehavior);
+            Assert.DoesNotContain(fixture.Behaviors, IsSpecimenBuilderTransformation<ThrowingRecursionBehavior>);
         }
 
-        [AssertionMethod]
         internal static void ShouldOmitRecursion(this IFixture fixture)
         {
             // Ensure there is a behavior added for omitting recursive types
             // on default recursion depth.
-            Assert.Single(fixture.Behaviors, b => b is OmitOnRecursionBehavior);
+            Assert.Single(fixture.Behaviors, IsSpecimenBuilderTransformation<OmitOnRecursionBehavior>);
         }
 
-        [AssertionMethod]
         internal static void ShouldNotIgnoreVirtualMembers(this IFixture fixture)
         {
-            Assert.DoesNotContain(fixture.Customizations, s => s is IgnoreVirtualMembersSpecimenBuilder);
+            Assert.DoesNotContain(fixture.Customizations, IsSpecimenBuilder<IgnoreVirtualMembersSpecimenBuilder>);
         }
 
-        [AssertionMethod]
         internal static void ShouldIgnoreVirtualMembers(this IFixture fixture)
         {
-            Assert.Single(fixture.Customizations, s => s is IgnoreVirtualMembersSpecimenBuilder);
+            Assert.Single(fixture.Customizations, IsSpecimenBuilder<IgnoreVirtualMembersSpecimenBuilder>);
         }
 
-        [AssertionMethod]
         internal static void ShouldIgnoreVirtualMembers(this IFixture fixture, Type reflectedType)
         {
             Assert.Single(
                 fixture.Customizations.OfType<IgnoreVirtualMembersSpecimenBuilder>(),
                 c => c.ReflectedType == reflectedType);
         }
+
+        private static bool IsSpecimenBuilderTransformation<T>(ISpecimenBuilderTransformation b)
+            where T : ISpecimenBuilderTransformation => b is T;
+
+        private static bool IsSpecimenBuilder<T>(ISpecimenBuilder s)
+            where T : ISpecimenBuilder => s is T;
     }
 }
